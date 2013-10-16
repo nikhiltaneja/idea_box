66adf3de5755f318916ac7f80c96808c1d3d59a6
Nikhil Taneja <nikhil.a.taneja@gmail.com>
2013-10-16 14:31:54 -0600

    I0 and I1

---
+++ app.rb
@@ -0,0 +1,22 @@
+require './idea'
+
+class IdeaBoxApp < Sinatra::Base
+
+  not_found do
+    erb :error
+  end
+  
+  configure :development do 
+    register Sinatra::Reloader
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/' do
+    idea = Idea.new(params['idea_title'], params['idea_description'])
+    idea.save
+    redirect '/'
+  end
+end
