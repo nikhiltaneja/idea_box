7169fc747d594362ff6bc30a330168ba0a7e3387
Nikhil Taneja <nikhil.a.taneja@gmail.com>
2013-10-16 21:21:09 -0600

    I2

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require './idea'
 
 class IdeaBoxApp < Sinatra::Base
+  set :method_override, true
 
   not_found do
     erb :error
@@ -11,7 +12,7 @@ class IdeaBoxApp < Sinatra::Base
   end
 
   get '/' do
-    erb :index
+    erb :index, locals: {ideas: Idea.all}
   end
 
   post '/' do
@@ -19,4 +20,24 @@ class IdeaBoxApp < Sinatra::Base
     idea.save
     redirect '/'
   end
+
+  delete '/:id' do |id|
+    Idea.delete(id.to_i)
+    redirect '/'
+  end
+
+  get '/:id/edit' do |id|
+    idea = Idea.find(id.to_i)
+    erb :edit, locals: {id: id, idea: idea}
+  end
+
+  put '/:id' do |id|
+    data = {
+      :title => params['idea_title'],
+      :description => params['idea_description']
+    }
+    Idea.update(id.to_i, data)
+    redirect '/'
+  end
+
 end
--- idea.rb
+++ idea.rb
@@ -15,7 +15,46 @@ class Idea
     end
   end
 
+  def self.database
+    @database ||= YAML::Store.new("ideabox")
+  end
+
   def database
-    @database ||= YAML::Store.new "ideabox"
+    Idea.database
+  end
+
+  def self.all
+    raw_ideas.map do |data|
+      new(data[:title], data[:description])
+    end
+  end
+
+  def self.raw_ideas
+    database.transaction do |db|
+      db['ideas'] || []
+    end
+  end
+
+  def self.delete(position)
+    database.transaction do
+      database['ideas'].delete_at(position)
+    end
+  end
+
+  def self.find(id)
+    raw_idea = find_raw_idea(id)
+    new(raw_idea[:title], raw_idea[:description])
+  end
+
+  def self.find_raw_idea(id)
+    database.transaction do
+      database['ideas'].at(id)
+    end
+  end
+
+  def self.update(id, data)
+    database.transaction do
+      database['ideas'][id] = data
+    end
   end
 end
