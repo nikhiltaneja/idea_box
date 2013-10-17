fec26532a2d54b589b4f27052cd039149bb9f33b
Nikhil Taneja <nikhil.a.taneja@gmail.com>
2013-10-17 11:17:53 -0600

    Added IdeaStore

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require './idea'
+require './idea_store'
 
 class IdeaBoxApp < Sinatra::Base
   set :method_override, true
@@ -12,31 +13,26 @@ class IdeaBoxApp < Sinatra::Base
   end
 
   get '/' do
-    erb :index, locals: {ideas: Idea.all}
+    erb :index, locals: {ideas: IdeaStore.all, idea: Idea.new}
   end
 
   post '/' do
-    idea = Idea.new(params['idea_title'], params['idea_description'])
-    idea.save
+    IdeaStore.create(params[:idea])
     redirect '/'
   end
 
   delete '/:id' do |id|
-    Idea.delete(id.to_i)
+    IdeaStore.delete(id.to_i)
     redirect '/'
   end
 
   get '/:id/edit' do |id|
-    idea = Idea.find(id.to_i)
+    idea = IdeaStore.find(id.to_i)
     erb :edit, locals: {id: id, idea: idea}
   end
 
   put '/:id' do |id|
-    data = {
-      :title => params['idea_title'],
-      :description => params['idea_description']
-    }
-    Idea.update(id.to_i, data)
+    IdeaStore.update(id.to_i, params[:idea])
     redirect '/'
   end
 
